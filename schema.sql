6d432dddf5afadbec5bf948c6e39b4b5c3063bd4
Tyler Lentini <tylernlentini@gmail.com>
2021-07-25 17:39:07 -0400

    start 7.3

--- schema.sql
+++ schema.sql
@@ -39,10 +39,10 @@ CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
+	to_date DATE NOT NULL, 
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
 	);
 
 CREATE TABLE titles (
@@ -50,8 +50,6 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 	);
 	
-SELECT * FROM departments;
